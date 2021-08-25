d095aa80c9c33318ab7758ceaa29c7442cb0a7fa
ceddral <mail@ceddral.org>
2021-08-25 17:51:34 +0200

    plant_cache: always check if node is valid

--- commands.lua
+++ commands.lua
@@ -13,7 +13,6 @@ local item_to_node = function(item)
 			if cropdef.seed == item then
 				node = crop .. "_1"
 				-- trust that farming.registered_plants only contains valid nodes
-				basic_robot.plant_cache[item] = node
 			end
 		end
 	end
@@ -23,6 +22,7 @@ local item_to_node = function(item)
 	if not minetest.registered_nodes[node] then
 		return nil
 	end
+	basic_robot.plant_cache[item] = node
 	return node
 end
 
